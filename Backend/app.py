566687fdc7eb69bb5d10a94ac02f7c80e1343944
Guillaume Dubrasquet-Duval <guillaume.dubrasquet-duval@etu.u-bordeaux.fr>
2023-06-22 14:16:00 +0200

    Fix issues and add unit test for CRUD

--- Backend/app.py
+++ Backend/app.py
@@ -2,20 +2,23 @@ from flask import Flask, jsonify, request
 from tinydb import TinyDB, Query
 from flask_cors import CORS
 from type.model import Model
-from logic import initialize_model
-import json
+#from logic import initialize_model
+import os
 import uuid
 
 app = Flask(__name__)
 CORS(app)
-db = TinyDB('../Database/database.json')
+current_dir = os.path.dirname(__file__)
+database_path = os.path.join(current_dir, '../Database/database.json')
+db = TinyDB(database_path)
 modelDB = db.table('modèles')
 
 model_test = Model(name='Modèle 1', nb_iterations=100, seed=123, type='local')
 
+#Initialise the model at the start of the server
 @app.before_request
-def comput_model():
-    print("test")
+def compute_model():
+    print("compute")
     #initialize_model()
 
 
@@ -27,17 +30,18 @@ def homepage():
 @app.route('/add', methods=['POST'])
 def add_model():
     data = request.get_json()
-
+    
+    model_id = data.get('id') if data.get('id') else None
     model_name = data.get('name')
     nb_iterations = data.get('nb_iterations')
     seed = data.get('seed')
     model_type = data.get('type')
-    new_model = Model(model_name,nb_iterations,seed,model_type)
+    new_model = Model(id = model_id, name = model_name,nb_iterations = nb_iterations,seed = seed,type = model_type)
     model_dict = new_model.to_dict()
 
     modelDB.insert({'model': model_dict})
 
-    return jsonify({'message': 'Model added successfully', 'id': model_dict["id"]}), 201
+    return jsonify({'message': 'Model added successfully', 'model': model_dict}), 201
 
 
 @app.route('/<id>', methods=['GET'])
@@ -45,7 +49,6 @@ def get_model_by_id(id):
 
     Model = Query()
     model = modelDB.get(Model.model.id == id)
-    print(model)
     if model:
         return jsonify({'model': model}), 200
     else:
@@ -58,7 +61,7 @@ def delete_model_by_id(id):
     model = modelDB.get(Model.model.id == id)
     if model:
         modelDB.remove(Model.model.id == id)
-        return jsonify({'message': 'Model deleted successfully'})
+        return jsonify({'message': 'Model deleted successfully', 'model': model}), 200
     else:
         return jsonify({'error': 'Model not found'}), 404
     
@@ -86,8 +89,9 @@ def update_model(id):
 
         modelDB.update(new_model, Model.model.id == id)
         updated_model = modelDB.get(Model.model.id == id)
+        print(updated_model)
     
-        return jsonify({'message': 'Model updated successfully'})
+        return jsonify({'message': 'Model updated successfully', 'model': updated_model['model']}), 200
     else:
         return jsonify({'error': 'Model not found'}), 404
 
@@ -114,16 +118,15 @@ def test_model():
     
     print("Test 4", dict_data)
  
-    return jsonify({'message': 'Model added successfully'})
+    return jsonify({'message': 'Test Ended'})
 
 @app.route('/list')
 def list_models():
     models = modelDB.all()
     return jsonify(models)
 
-@app.route('/entrainer')
+@app.route('/train')
 def entrainer_modele():
-    # Logique d'entraînement du modèle
     return jsonify({'message': 'Entraînement terminé'})
 
 @app.route('/inference', methods=['POST'])
---
+++ Backend/test/app_test.py
@@ -0,0 +1,98 @@
+import json
+import sys, os
+import unittest
+import time
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+from app import app
+
+
+
+class CRUDTests(unittest.TestCase):
+            
+    model_dict = {
+                'id' : '1',
+                'name': 'Test Model',
+                'nb_iterations': 100,
+                'seed': 123,
+                'type': 'local'
+                }
+
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+
+    def tearDown(self):
+        # Clear Database if needed 
+        pass
+
+    def test1_add_model(self):
+        print("test_add_model")
+        response = self.app.post('/add', json=self.model_dict)
+        self.assertEqual(response.status_code, 201)
+        response_data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response_data['message'], 'Model added successfully')
+        print(response_data)        
+        self.assertEqual(response_data['model']['name'], self.model_dict['name'])
+
+    def test2_get_model_by_id(self):
+        print("test_get_model_by_id")
+        response = self.app.get('/1')
+        self.assertEqual(response.status_code, 200)
+        response_data = json.loads(response.get_data(as_text=True))
+        print(response_data)
+        self.assertIn('model', response_data)
+
+    def test3_get_model_by_invalid_id(self):
+        print("test_get_model_by_invalid_id")
+        response = self.app.get('/100')
+        self.assertEqual(response.status_code, 404)
+        response_data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response_data['error'], 'Model not found')
+
+    def test4_update_model(self):
+        print("test_update_model")
+        data = {
+            'name': 'Updated Model',
+            'nb_iterations': 200,
+            'seed': 456,
+            'type': 'mixte'
+        }
+        response = self.app.put('/update/1', json=data)
+        self.assertEqual(response.status_code, 200)
+        response_data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response_data['message'], 'Model updated successfully')
+        print(response_data['model']['name'], data['name'])
+        self.assertEqual(response_data['model']['name'], data['name'])
+
+    def test5_update_model_with_invalid_id(self):
+        print("test_update_model_with_invalid_id")
+        data = {
+            'name': 'Updated Model',
+            'nb_iterations': 200,
+            'seed': 456,
+            'type': 'mixte'
+        }
+        response = self.app.put('/update/100', json=data)
+        self.assertEqual(response.status_code, 404)
+        response_data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response_data['error'], 'Model not found')
+        
+    def test6_delete_model_by_id(self):
+        response = self.app.delete('/1')
+        self.assertEqual(response.status_code, 200)
+        response_data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response_data['message'], 'Model deleted successfully')
+
+    def test7_delete_model_by_invalid_id(self):
+        response = self.app.delete('/100')
+        self.assertEqual(response.status_code, 404)
+        response_data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response_data['error'], 'Model not found')
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
